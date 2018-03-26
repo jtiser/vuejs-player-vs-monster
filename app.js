46b2d67da634ce2763fa7c801b7f4cb8db0dca7f
Jtiser <julien.tiser@outlook.com>
2018-03-26 16:25:18 +0200

    Implemeting a "Heal" Method

--- app.js
+++ app.js
@@ -26,11 +26,16 @@ new Vue({
             if(this.checkWin()) {
                 return;
             }
-            
+
             this.monsterAttacks();
         },
         heal: function() {
-
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         giveUp: function() {
 
